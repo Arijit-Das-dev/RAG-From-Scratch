ee43a332f1a256e60a7fe40ef22085052de89da2
Arijit Das <arijitdas.das99@gmail.com>
2026-01-29 14:26:04 +0530

    Updated

--- IngestionPipeLine/main.py
+++ IngestionPipeLine/main.py
@@ -15,10 +15,43 @@ Steps =>
 
 """
 
+# loading all the files
+
+def load_all_files(file_path = "Docs"):
+
+    print(f"LOADING ALL THE FILES FROM {file_path}")
+
+    if not os.path.exists(file_path): 
+
+        raise FileNotFoundError(f"{file_path} DOES NOT EXISTS")
+    
+    loader = DirectoryLoader(
+
+        path=file_path,
+        glob="*.txt",
+        loader_cls=TextLoader
+    )
+    
+    print("_______FILE LOADED SUCCESSFULLY_______")
+
+    # Loading all the documents [.txt] from Docs folder
+    documents = loader.load()
+
+    if len(documents) == 0:
+
+        raise FileNotFoundError(f"{file_path} CONTAINS NOTHING. ENSURE THAT FILE IS NOT EMPTY.")
+    
+    for i, doc in documents[:1]:
+
+        print(f"File no. {i+1}")
+
+    return documents
+
+
 def main():
 
     print("Main Function")
-    
+
 
 
 if __name__ == "__main__":
